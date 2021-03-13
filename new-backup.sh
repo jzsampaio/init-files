8b5d07dfe34bf90e060fac537fe0499d5052015a
Juarez Sampaio <juarez.asf@gmail.com>
2021-03-13 07:48:24 -0300

    Add backup created at 2021-03-13T10:48:24Z

--- new-backup.sh
+++ new-backup.sh
@@ -4,6 +4,7 @@ cp ~/.emacs.d/init.el emacs.d/
 rsync -r ~/.emacs.d/autoload/ emacs.d/autoload
 cp ~/.zshrc zshrc
 cp ~/.gitconfig gitconfig
+crontab -l > user-crontab
 
 git add .
 git commit -m "Add backup created at $(TZ=GMT date +'%Y-%m-%dT%H:%M:%SZ')" && git push origin master
