46dccfda20438bbfb906c80c571aa0af965f377d
Juarez Sampaio <juarez.asf@gmail.com>
2022-07-29 10:24:11 -0300

    Add backup created at 2022-07-29T13:24:11Z

--- new-backup.sh
+++ new-backup.sh
@@ -4,6 +4,7 @@ cp ~/.emacs.d/init.el emacs.d/
 cp ~/.emacs.d/settings.org  emacs.d/
 rsync -r ~/.emacs.d/autoload/ emacs.d/autoload
 rsync -r ~/.emacs.d/lisp/ emacs.d/lisp
+rsync -r ~/.emacs.d/org-templates/ emacs.d/org-templates
 cp ~/.zshrc zshrc
 cp ~/.gitconfig gitconfig
 crontab -l > user-crontab
